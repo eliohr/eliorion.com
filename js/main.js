7ed456c5f0814924ce42ad827f1b15771a42bb2b
Eli Hooker Reese <ellius.eli@gmail.com>
2024-08-01 20:53:17 -0500

    clean up commit 1: rename/resize images, reorganize directories, clarify home. to do: clarify index, listen, indexfalse, and all landings (including landings-styles—stylesheets are being clarified along the way with their referencing pages)

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+/*
+function autoplayAmbience {
+    const audio = document.querySelector('audio');
+    const bandcampEmbed = document.querySelector('iframe[src*="bandcamp"]');
+    const youtubeEmbed = document.querySelector('iframe[src*="youtube"]');
+    const spotifyEmbed = document.querySelector('iframe[src*="spotify"]');
+    
+    bandcampEmbed.addEventListener('mouseover', function() {
+        audio.pause();
+    });
+
+    youtubeEmbed.addEventListener('mouseover', function() {
+        audio.pause();
+    });
+
+    spotifyEmbed.addEventListener('mouseover', function() {
+        audio.pause();
+    });
+});
+document.addEventListener('DOMContentLoaded', autoplayAmbience);
+*/
+
+// top section background selector
+function setTopBackground() {
+    const top = document.querySelector('.top');
+    const randomNum = Math.floor(Math.random() * 6) + 1;
+    top.classList.add('img' + randomNum); 
+}
+document.addEventListener('DOMContentLoaded', setTopBackground);
+
+// flipper scripter
+function imageFlipper() {
+    document.getElementById("flipper").onmouseover = function() {
+        this.src = "media/images/facepalm.jpeg";
+    }
+    document.getElementById("flipper").onmouseout = function() {
+        this.src = "media/images/vest.jpeg";
+    }
+}
+document.addEventListener('DOMContentLoaded', imageFlipper);
